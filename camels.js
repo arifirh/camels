96ef5a447d361d8f170b326d222223a49218e088
Phil Scott <arifirh@gmail.com>
2013-08-31 12:07:12 +0100

    randomCamelWorth and randomResponse now work

--- camels.js
+++ camels.js
@@ -2,11 +2,27 @@ function coinToss() {
 	return Math.floor(Math.random() * 2) === 1;
 }
 
+function numberBetween(min, max) {
+	return Math.random() * (max - min) + min;
+}
+
+function pickKeyFromObject(object) {
+	keys = Object.keys(object);
+	index = Math.floor(numberBetween(0, keys.length));
+	return object[keys[index]];
+}
+
+function keepPickingKeysUntilString(input) {
+	if (typeof input === 'string') {
+		return input;
+	}
+	else { 
+		return keepPickingKeysUntilString(pickKeyFromObject(input));
+	}
+}
+
 function someNumber(size) {
 	var sigFigs;
-	var randomBetweenRange = function(min, max) {
-		return Math.random() * (max - min) + min;
-	}
 	switch(size) {
 		case 'verySmall':
 			if (coinToss()) {
@@ -15,67 +31,74 @@ function someNumber(size) {
 			else {
 				sigFigs = 2;
 			}
-			return (randomBetweenRange(0.1, 0.9)).toFixed(sigFigs);
+			return (numberBetween(0.1, 0.9)).toFixed(sigFigs);
 		default:
 			return '3';
 	}
 }
 
 var camelWorths = {
-	'none': 'no',
-	'barelyAny': [
-		'barely any',
-		someNumber('verySmall')
-		],
-	'one': 'one',
-	'some': someNumber('small'),
-	'lots': someNumber('large'),
-	'anAwfulLot': 'such an awful lot of',
+	typesOfWorth: {
+		'none': 'no',
+		'barelyAny': [
+			'barely any',
+			'not very many',
+			someNumber('verySmall')
+			],
+		'one': 'one',
+		'some': someNumber('small'),
+		'lots': someNumber('large'),
+		'anAwfulLot': 'such an awful lot of',
+	},
 	randomCamelWorth: function() {
-		return 'not very many';
+		return keepPickingKeysUntilString(camelWorths['typesOfWorth']);
 	}
 }
 
 var responses = {
-	'none': 'Go home',
-	'barelyAny': [
-		'Well, nevermind',
-		'I guess that\'s not too bad',
-		'You still have your health',
-		'It\'s really nothing to be ashamed of'
-		],
-	'some': [
-		'That\'s quite respectable, really',
-		'Well done',
-		'Good for you'
-		],
-	'lots': [
-		'Gosh',
-		'Heavens',
-		'Gracious me',
-		'Well done',
-		'What a lot of camels'
-		],
-	'anAwfulLot': [
-		'I\'m actually slightly embarrased to say',
-		'I couldn\'t possibly tell you, you\'d get a big head',
-		'I started counting them but I got bored'
-		],
+	typesOfResponse: {
+		'none': 'Go home',
+		'barelyAny': [
+			'Well, nevermind',
+			'I guess that\'s not too bad',
+			'You still have your health',
+			'It\'s really nothing to be ashamed of'
+			],
+		'some': [
+			'That\'s quite respectable, really',
+			'Well done',
+			'Good for you'
+			],
+		'lots': [
+			'Gosh',
+			'Heavens',
+			'Gracious me',
+			'Well done',
+			'What a lot of camels'
+			],
+		'anAwfulLot': [
+			'I\'m actually slightly embarrased to say',
+			'I couldn\'t possibly tell you, you\'d get a big head',
+			'I started counting them but I got bored'
+			],
+	},
 	randomResponse: function() {
-		return 'Go home';
+		return keepPickingKeysUntilString(responses['typesOfResponse']);
 	}
 }
 
 var buttons = {
 	buttonText: [
 		'Rubbish',
-		'What nonsense',
+		'What',
 		'Utter bollocks',
-		'Sod off, no I\'m not',
-		'I am worth at least twice that many camels'
-			],
+		'Sod off no I\'m not',
+		'I am worth at least twice that many camels',
+		'Load of toss',
+		'You are talking out of your hat'
+		],
 	randomButton: function() {
-		return 'Rubbish';
+		return pickKeyFromObject(buttons['buttonText']);
 	}
 }
 
